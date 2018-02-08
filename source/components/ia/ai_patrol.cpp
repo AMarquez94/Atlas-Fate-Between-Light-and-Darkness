28a2ac6fd165437e220eae557dece77a10e486f0
Alejandro Márquez <alejandromarquez94@gmail.com>
2018-02-08 13:58:40 +0100

    WIP: ai_patrol working (with minor flaws)

--- source/components/ia/ai_patrol.cpp
+++ source/components/ia/ai_patrol.cpp
@@ -13,7 +13,15 @@ void CAIPatrol::Init()
   AddState("idle", (statehandler)&CAIPatrol::IdleState);
   AddState("seekwpt", (statehandler)&CAIPatrol::SeekWptState);
   AddState("nextwpt", (statehandler)&CAIPatrol::NextWptState);
+  AddState("closestwpt", (statehandler)&CAIPatrol::ClosestWptState);
   AddState("chase", (statehandler)&CAIPatrol::ChaseState);
+  AddState("idleWar", (statehandler)&CAIPatrol::IdleWarState);
+  AddState("chooseOrbitSide", (statehandler)&CAIPatrol::ChooseOrbitSideState);
+  AddState("orbitLeft", (statehandler)&CAIPatrol::OrbitLeftState);
+  AddState("orbitRight", (statehandler)&CAIPatrol::OrbitRightState);
+  AddState("back", (statehandler)&CAIPatrol::BackState);
+  AddState("hit", (statehandler)&CAIPatrol::HitState);
+  AddState("dead", (statehandler)&CAIPatrol::DeadState);
 
   // reset the state
   ChangeState("idle");
@@ -48,10 +56,18 @@ void CAIPatrol::load(const json& j, TEntityParseContext& ctx) {
 
 }
 
+void CAIPatrol::registerMsgs() {
+	DECL_MSG(CAIPatrol, TMsgDamage, onMsgDamage);
+}
+
+void CAIPatrol::onMsgDamage(const TMsgDamage& msg) {
+	ChangeState("hit");
+}
+
 
 void CAIPatrol::IdleState(float dt)
 {
-  CEntity *player = (CEntity *)getEntityByName("The Player");
+  CEntity *player = (CEntity *)getEntityByName(entityToChase);
   TCompTransform *mypos = getMyTransform();
   TCompTransform *ppos = player->get<TCompTransform>();
   bool in_fov = mypos->isInFov(ppos->getPosition(), deg2rad(60));
@@ -66,25 +82,25 @@ void CAIPatrol::SeekWptState(float dt)
   mypos->getYawPitchRoll(&y, &p, &r);
   if (mypos->isInLeft(getWaypoint()))
   {
-    y += 0.001f;
+    y += rotationSpeed * dt;
   }
   else
   {
-    y -= 0.001f;
+    y -= rotationSpeed * dt;
   }
   mypos->setYawPitchRoll(y, p, r);
   VEC3 vp = mypos->getPosition();
   VEC3 vfwd = mypos->getFront();
   vfwd.Normalize();
-  vp = vp + 0.001f*vfwd;
+  vp = vp + speed * dt *vfwd;
   mypos->setPosition(vp);
   // next wpt
   if (VEC3::Distance(getWaypoint(), vp) < 1) ChangeState("nextwpt");
 
   // chase
-  CEntity *player = (CEntity *)getEntityByName("The Player");
+  CEntity *player = (CEntity *)getEntityByName(entityToChase);
   TCompTransform *ppos = player->get<TCompTransform>();
-  bool in_fov = mypos->isInFov(ppos->getPosition(), deg2rad(60));
+  bool in_fov = mypos->isInFov(ppos->getPosition(), fov);
   if (in_fov) ChangeState("chase");
 }
 
@@ -94,30 +110,207 @@ void CAIPatrol::NextWptState(float dt)
   ChangeState("seekwpt");
 }
 
+
+void CAIPatrol::ClosestWptState(float dt) {
+	float minDistance = INFINITY;
+	int  minIndexWpt = 0;
+
+	for (int i = 0; i < _waypoints.size(); i++) {
+		float currDistance = VEC3::Distance(getMyTransform()->getPosition(), _waypoints[i]);
+		if (currDistance < minDistance) {
+			minDistance = currDistance;
+			minIndexWpt = i;
+		}
+	}
+
+	currentWaypoint = minIndexWpt;
+	ChangeState("seekwpt");
+}
+
+
 void CAIPatrol::ChaseState(float dt)
 {
   TCompTransform *mypos = getMyTransform();
-  CEntity *player = (CEntity *)getEntityByName("The Player");
+  CEntity *player = (CEntity *)getEntityByName(entityToChase);
   TCompTransform *ppos = player->get<TCompTransform>();
   float y, r, p;
   mypos->getYawPitchRoll(&y, &p, &r);
   if (mypos->isInLeft(ppos->getPosition()))
   {
-    y += 0.001f;
+    y += rotationSpeed * dt;
   }
   else
   {
-    y -= 0.001f;
+    y -= rotationSpeed * dt;
   }
   mypos->setYawPitchRoll(y, p, r);
   VEC3 vp = mypos->getPosition();
   VEC3 vfwd = mypos->getFront();
   vfwd.Normalize();
-  vp = vp + 0.001f*vfwd;
+  vp = vp + speed * dt * vfwd;
   mypos->setPosition(vp);
 
-  bool in_fov = mypos->isInFov(ppos->getPosition(), deg2rad(60));
-  if (!in_fov) ChangeState("seekwpt");
+  bool in_fov = mypos->isInFov(ppos->getPosition(), fov);
+  if (!in_fov) ChangeState("closestwpt");
 
+  if (VEC3::Distance(mypos->getPosition(), ppos->getPosition()) <= distToIdleWar) {
+	  idleWarTimerMax = idleWarTimerBase + (rand() % idleWarTimerExtra);
+	  ChangeState("idleWar");
+  }
+
+}
+
+
+void CAIPatrol::IdleWarState(float dt) {
+	TCompTransform *mypos = getMyTransform();
+	CEntity *player = (CEntity *)getEntityByName(entityToChase);
+	TCompTransform *ppos = player->get<TCompTransform>();
+
+	if (VEC3::Distance(mypos->getPosition(), ppos->getPosition()) <= distToBack) {
+		idleWarTimer = 0;
+		ChangeState("back");
+	}
+	else if(VEC3::Distance(mypos->getPosition(), ppos->getPosition()) > distToIdleWar + 0.5){
+		idleWarTimer = 0;
+		ChangeState("chase");
+	}
+	else {
+
+		float y, r, p;
+		mypos->getYawPitchRoll(&y, &p, &r);
+		if (mypos->isInLeft(ppos->getPosition()))
+		{
+			y += rotationSpeed * dt;
+		}
+		else
+		{
+			y -= rotationSpeed * dt;
+		}
+		mypos->setYawPitchRoll(y, p, r);
+
+		if (idleWarTimer >= idleWarTimerMax) {
+			idleWarTimer = 0;
+			ChangeState("chooseOrbitSide");
+		}
+		else {
+			idleWarTimer += dt;
+		}
+	}
+}
+
+
+void CAIPatrol::ChooseOrbitSideState(float dt) {
+	orbitRotationMax = orbitRotationBase + deg2rad(rand() % orbitRotationExtra);
+	if (rand() % 2 == 0) {
+		ChangeState("orbitLeft");
+	}
+	else {
+		ChangeState("orbitRight");
+	}
+
+}
+
+
+void CAIPatrol::OrbitLeftState(float dt) {
+	TCompTransform *mypos = getMyTransform();
+	CEntity *player = (CEntity *)getEntityByName(entityToChase);
+	TCompTransform *ppos = player->get<TCompTransform>();
+
+	if (VEC3::Distance(mypos->getPosition(), ppos->getPosition()) <= distToBack) {
+		currentOrbitRotation = 0;
+		ChangeState("back");
+	}
+	else {
+		float distance = VEC3::Distance(ppos->getPosition(), mypos->getPosition());
+		float y, r, p;
+		mypos->getYawPitchRoll(&y, &p, &r);
+
+		mypos->setPosition(ppos->getPosition());
+		float deltaYaw = (rotationSpeed * dt);
+		currentOrbitRotation = currentOrbitRotation + deltaYaw;
+		mypos->setYawPitchRoll(y - deltaYaw, p, r);
+
+		mypos->setPosition(mypos->getPosition() - mypos->getFront() * distance);
+
+		if (currentOrbitRotation >= orbitRotationMax) {
+			currentOrbitRotation = 0;
+			idleWarTimerMax = idleWarTimerBase + (rand() % idleWarTimerExtra);
+			ChangeState("idleWar");
+		}
+	}
+}
+
+
+void CAIPatrol::OrbitRightState(float dt) {
+	TCompTransform *mypos = getMyTransform();
+	CEntity *player = (CEntity *)getEntityByName(entityToChase);
+	TCompTransform *ppos = player->get<TCompTransform>();
+
+	if (VEC3::Distance(mypos->getPosition(), ppos->getPosition()) <= distToBack) {
+		currentOrbitRotation = 0;
+		ChangeState("back");
+	}
+	else {
+		float distance = VEC3::Distance(ppos->getPosition(), mypos->getPosition());
+		float y, r, p;
+		mypos->getYawPitchRoll(&y, &p, &r);
+
+		mypos->setPosition(ppos->getPosition());
+		float deltaYaw = (rotationSpeed * dt);
+		currentOrbitRotation = currentOrbitRotation + deltaYaw;
+		mypos->setYawPitchRoll(y + deltaYaw, p, r);
+
+		mypos->setPosition(mypos->getPosition() - mypos->getFront() * distance);
+
+		if (currentOrbitRotation >= orbitRotationMax) {
+			currentOrbitRotation = 0;
+			idleWarTimerMax = idleWarTimerBase + (rand() % idleWarTimerExtra);
+			ChangeState("idleWar");
+		}
+	}
+}
+
+
+void CAIPatrol::BackState(float dt) {
+	TCompTransform *mypos = getMyTransform();
+	CEntity *player = (CEntity *)getEntityByName(entityToChase);
+	TCompTransform *ppos = player->get<TCompTransform>();
+	float y, r, p;
+	mypos->getYawPitchRoll(&y, &p, &r);
+	if (mypos->isInLeft(ppos->getPosition()))
+	{
+		y += rotationSpeed * dt;
+	}
+	else
+	{
+		y -= rotationSpeed * dt;
+	}
+	mypos->setYawPitchRoll(y, p, r);
+	VEC3 vp = mypos->getPosition();
+	VEC3 vfwd = mypos->getFront();
+	vfwd.Normalize();
+	vp = vp - speed * dt * vfwd;
+	mypos->setPosition(vp);
+
+	if (VEC3::Distance(mypos->getPosition(), ppos->getPosition()) > distToBack + 0.5f) {
+		idleWarTimerMax = idleWarTimerBase + (rand() % idleWarTimerExtra);
+		ChangeState("idleWar");
+	}
+}
+
+
+void CAIPatrol::HitState(float dt) {
+	 life = life - 1;
+	 if (life <= 0) {
+		ChangeState("dead");
+	 }
+	 else {
+		 ChangeState("closestwpt");
+	 }
+}
+
+void CAIPatrol::DeadState(float dt) {
+	fatal("You are dead madafaka");
+	exit(ERROR_VIRUS_INFECTED);
 }
 
--- source/components/ia/ai_patrol.h
+++ source/components/ia/ai_patrol.h
@@ -2,13 +2,35 @@
 #define _AIC_PATROL
 
 #include "ai_controller.h"
+#include "entity/common_msgs.h"
 
 class CAIPatrol : public IAIController
 {
+
+	/* Atributes */
   std::vector<VEC3> _waypoints;
   int currentWaypoint;
+  float speed = 2.f;
+  float rotationSpeed = deg2rad(90);
+  float fov = deg2rad(60);
+  float distToIdleWar = 2.f;
+  float distToBack = 1.f;
+  std::string entityToChase = "The Player";
+  int life = 5;
+
+  /* Timers */
+  float idleWarTimer = 0.f;
+  float idleWarTimerBase = 1.f;
+  int idleWarTimerExtra = 2;
+  float idleWarTimerMax = 0.f;
+  float currentOrbitRotation = 0.f;
+  float orbitRotationBase = deg2rad(60);
+  int orbitRotationExtra = 30;
+  float orbitRotationMax = 0.f;
 
   DECL_SIBLING_ACCESS();
+
+  void onMsgDamage(const TMsgDamage& msg);
 
 public:
   void load(const json& j, TEntityParseContext& ctx);
@@ -17,12 +39,22 @@ public:
   void IdleState(float);
   void SeekWptState(float);
   void NextWptState(float);
+  void ClosestWptState(float);
   void ChaseState(float);
+  void IdleWarState(float);
+  void ChooseOrbitSideState(float);
+  void OrbitLeftState(float);
+  void OrbitRightState(float);
+  void BackState(float);
+  void HitState(float);
+  void DeadState(float);
 
   void Init();
 
   void addWaypoint(VEC3 waypoint) { _waypoints.push_back(waypoint); };
   VEC3 getWaypoint() { return _waypoints[currentWaypoint]; }
+
+  static void registerMsgs();
 };
 
 #endif
