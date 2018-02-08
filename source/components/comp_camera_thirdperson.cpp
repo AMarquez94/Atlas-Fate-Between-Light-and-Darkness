598302c59206a92770b9bb9a4eb6e9247135faf4
vostek <alberto93.dev@gmail.com>
2018-02-08 23:20:56 +0100

    FEATURE: Adding the camera controller to the camera.

--- source/components/comp_camera_thirdperson.cpp
+++ source/components/comp_camera_thirdperson.cpp
@@ -1,28 +1,41 @@
 #include "mcv_platform.h"
-#include "comp_camera.h"
+#include "entity/entity_parser.h"
+#include "comp_camera_thirdperson.h"
 #include "comp_transform.h"
 
-DECL_OBJ_MANAGER("camera", TCompCamera);
+DECL_OBJ_MANAGER("camera_thirdperson", TCompCameraThirdPerson);
 
-void TCompCamera::debugInMenu() {
-
+void TCompCameraThirdPerson::debugInMenu()
+{
+	ImGui::DragFloat("Vertical offset", &_clipping_offset.x, 0.1f, 0.f, 20.f);
+	ImGui::DragFloat("Horizontal offset", &_clipping_offset.y, 0.1f, 0.f, 20.f);
 }
 
-void TCompCamera::load(const json& j, TEntityParseContext& ctx) {
-
-  // ..
-  float fov_deg = j.value("fov", 75.f);
-  float z_near = j.value("z_near", 0.1f);
-  float z_far = j.value("z_far", 1000.f);
-  setPerspective(deg2rad(fov_deg), z_near, z_far);
-
+void TCompCameraThirdPerson::load(const json& j, TEntityParseContext& ctx)
+{
+	_target_name = j.value("target", "");
+	_clipping_offset = loadVEC2(j["offset"]);
+	_h_target = ctx.findEntityByName(_target_name);
 }
 
-void TCompCamera::update(float dt) {
+void TCompCameraThirdPerson::update(float dt)
+{
+	TCompTransform* self_transform = get<TCompTransform>();
+	TCompTransform* target_transform = ((CEntity*)_h_target)->get<TCompTransform>();
+	assert(self_transform);
+	assert(target_transform);
 
-  TCompTransform* c = get<TCompTransform>();
-  assert(c);
-  this->lookAt(c->getPosition(), c->getPosition() + c->getFront(), c->getUp());
+	VEC3 vertical_offset = _clipping_offset.x * VEC3::Up;
+	VEC3 horizontal_offset = _clipping_offset.y * -target_transform->getFront();
+	VEC3 newpos = target_transform->getPosition() + vertical_offset + horizontal_offset;//target_transform->getPosition() + _clipping_offset * angled_director;
+	VEC3 target_forwarded = target_transform->getPosition() + target_transform->getFront() * 4.0f;
 
+	self_transform->lookAt(newpos, target_forwarded);
 }
 
+void TCompCameraThirdPerson::mouseMovement(void)
+{
+	//VEC2 prev_cursor = cursor;
+	//cursor = VEC2(ImGui::GetMousePos().x, ImGui::GetMousePos().y);
+	//VEC2 delta_cursor = cursor - prev_cursor;
+}
--- source/components/comp_camera_thirdperson.h
+++ source/components/comp_camera_thirdperson.h
@@ -2,11 +2,21 @@
 
 #include "comp_base.h"
 #include "camera/camera.h"
+#include "entity/common_msgs.h"
 
-class TCompCamera : public CCamera, public TCompBase {
+class TCompCameraThirdPerson : public CCamera, public TCompBase
+{
+private:
+	CHandle     _h_target;
+	std::string _target_name;
+	VEC2 _clipping_offset;
+
 public:
   void debugInMenu();
   void load(const json& j, TEntityParseContext& ctx);
   void update(float dt);
+
+  void mouseMovement(void);
+
   DECL_SIBLING_ACCESS();
 };
