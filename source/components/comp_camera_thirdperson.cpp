feaeec6138ad00458b6a3a92d25655c9d58ccfc2
vostek <alberto93.dev@gmail.com>
2018-02-09 16:15:57 +0100

    FEATURE: basic camera controller component finished.

--- source/components/comp_camera_thirdperson.cpp
+++ source/components/comp_camera_thirdperson.cpp
@@ -4,38 +4,70 @@
 #include "comp_transform.h"
 
 DECL_OBJ_MANAGER("camera_thirdperson", TCompCameraThirdPerson);
+const Input::TInterface_Mouse& mouse = EngineInput.mouse();
 
 void TCompCameraThirdPerson::debugInMenu()
 {
-	ImGui::DragFloat("Vertical offset", &_clipping_offset.x, 0.1f, 0.f, 20.f);
-	ImGui::DragFloat("Horizontal offset", &_clipping_offset.y, 0.1f, 0.f, 20.f);
+	ImGui::DragFloat3("Offsets", &_clipping_offset.x, 0.1f, -20.f, 20.f);
+	ImGui::DragFloat2("Angles", &_clamp_angle.x, 0.1f, -90.f, 90.f);
+	ImGui::DragFloat("Speed", &_speed, 0.1f, 0.f, 20.f);
 }
 
 void TCompCameraThirdPerson::load(const json& j, TEntityParseContext& ctx)
 {
+	// Read from the json all the input data
+	_speed = j.value("speed", 3.0f);
 	_target_name = j.value("target", "");
-	_clipping_offset = loadVEC2(j["offset"]);
+	_clamp_angle = loadVEC2(j["clampangle"]);
+	_clipping_offset = loadVEC3(j["offset"]);
+	_clamp_angle = VEC2(deg2rad(_clamp_angle.x), deg2rad(_clamp_angle.y));
+
+	// Load the target and set his axis as our axis.
+	float yaw, pitch, roll;
 	_h_target = ctx.findEntityByName(_target_name);
+	TCompTransform* target_transform = ((CEntity*)_h_target)->get<TCompTransform>();
+	target_transform->getYawPitchRoll(&yaw, &pitch, &roll);
+	_current_euler = VEC2(yaw, pitch);
 }
 
 void TCompCameraThirdPerson::update(float dt)
 {
+	if (!_h_target.isValid())
+		return;
+
 	TCompTransform* self_transform = get<TCompTransform>();
-	TCompTransform* target_transform = ((CEntity*)_h_target)->get<TCompTransform>();
+	TCompTransform* target_transform = ((CEntity*)_h_target)->get<TCompTransform>(); // we will need to consume this.
 	assert(self_transform);
 	assert(target_transform);
 
-	VEC3 vertical_offset = _clipping_offset.x * VEC3::Up;
-	VEC3 horizontal_offset = _clipping_offset.y * -target_transform->getFront();
-	VEC3 newpos = target_transform->getPosition() + vertical_offset + horizontal_offset;//target_transform->getPosition() + _clipping_offset * angled_director;
-	VEC3 target_forwarded = target_transform->getPosition() + target_transform->getFront() * 4.0f;
+	// Verbose code
+	_current_euler.x += mouse._position_delta.x * _speed * 0.001f;
+	_current_euler.y -= mouse._position_delta.y * _speed * 0.001f;
+	_current_euler.y = std::max(-_clamp_angle.y, std::min(_current_euler.y, -_clamp_angle.x)); // clamp(y, _clamp_angle.x, _clamp_angle.y);
 
-	self_transform->lookAt(newpos, target_forwarded);
+	// EulerAngles method based on mcv class
+	VEC3 vertical_offset = VEC3::Up * _clipping_offset.y; // Change VEC3::up, for the players vertical angle, (TARGET VERTICAL)
+	VEC3 horizontal_offset = self_transform->getLeft() * _clipping_offset.x;
+	VEC3 target_position = target_transform->getPosition() + vertical_offset + horizontal_offset;
+
+	self_transform->setPosition(target_position);
+	self_transform->setYawPitchRoll(_current_euler.x, _current_euler.y, 0);
+	VEC3 new_pos = target_position + _clipping_offset.z * -self_transform->getFront();
+	self_transform->setPosition(new_pos);
+
+	// Quaternion method based on Unity prototype, to-do
+	//_mouse_pos.y = clamp(y, _clamp_angle.x, _clamp_angle.y);
+	//Quaternion rotation = Quaternion::CreateFromYawPitchRoll(_mouse_pos.y, _mouse_pos.x, 0);
+	//VEC3 position = (VEC3)(rotation * VEC3(0.0, 0.0, -5)) + target_transform->getPosition();
+	//self_transform->setRotation(rotation);
+	//self_transform->setPosition(position + VEC3(0,4,0));
 }
 
-void TCompCameraThirdPerson::mouseMovement(void)
+VEC3 TCompCameraThirdPerson::CameraClipping(void)
 {
-	//VEC2 prev_cursor = cursor;
-	//cursor = VEC2(ImGui::GetMousePos().x, ImGui::GetMousePos().y);
-	//VEC2 delta_cursor = cursor - prev_cursor;
+	//TO-DO
+	// Raycast from player direction to desired camera pos
+	// Return the position
+
+	return VEC3();
 }
--- source/components/comp_camera_thirdperson.h
+++ source/components/comp_camera_thirdperson.h
@@ -9,14 +9,18 @@ class TCompCameraThirdPerson : public CCamera, public TCompBase
 private:
 	CHandle     _h_target;
 	std::string _target_name;
-	VEC2 _clipping_offset;
+
+	float _speed;
+	VEC2 _clamp_angle;
+	VEC2 _current_euler;
+	VEC3 _clipping_offset;
 
 public:
   void debugInMenu();
   void load(const json& j, TEntityParseContext& ctx);
   void update(float dt);
 
-  void mouseMovement(void);
+  VEC3 CameraClipping(void);
 
   DECL_SIBLING_ACCESS();
 };
