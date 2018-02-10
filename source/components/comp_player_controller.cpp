5c7f9f9741754bf30327cc4d24836b4f5f5eb422
Alejandro Márquez <alejandromarquez94@gmail.com>
2018-02-10 19:24:37 +0100

    WIP: Player working with our inputs.

--- source/components/comp_player_controller.cpp
+++ source/components/comp_player_controller.cpp
@@ -2,18 +2,19 @@
 #include "entity/entity_parser.h"
 #include "comp_player_controller.h"
 #include "comp_transform.h"
+#include "comp_render.h"
 #include "entity/common_msgs.h"
 
 DECL_OBJ_MANAGER("player_controller", TCompPlayerController);
 
 void TCompPlayerController::debugInMenu() {
-  ImGui::DragFloat("Speed", &speedFactor, 0.1f, 0.f, 20.f);
+  ImGui::DragFloat("Speed", &walkSpeedFactor, 0.1f, 0.f, 20.f);
   ImGui::DragFloat("Rotation", &rotationSpeed, 0.1f, 0.f, 20.f);
   ImGui::Text("State: %s", stateName.c_str());
 }
 
 void TCompPlayerController::load(const json& j, TEntityParseContext& ctx) {
-  speedFactor = j.value("speed", 1.0f);
+  walkSpeedFactor = j.value("speed", 1.0f);
   rotationSpeed = j.value("rotation_speed", 1.0f);
 
   Init();
@@ -28,68 +29,70 @@ void TCompPlayerController::Init() {
 	AddState("smEnemy", (statehandler)&TCompPlayerController::ShadowMergingEnemyState);
 	AddState("smLand", (statehandler)&TCompPlayerController::ShadowMergingLandingState);
 	AddState("smExit", (statehandler)&TCompPlayerController::ShadowMergingExitState);
-	AddState("push", (statehandler)&TCompPlayerController::PushState);
 	AddState("attack", (statehandler)&TCompPlayerController::AttackState);
 	AddState("rmInhibitor", (statehandler)&TCompPlayerController::RemovingInhibitorState);
-	AddState("probe", (statehandler)&TCompPlayerController::ProbeState);
 	AddState("fall", (statehandler)&TCompPlayerController::FallingState);
 	AddState("land", (statehandler)&TCompPlayerController::LandingState);
 	AddState("hit", (statehandler)&TCompPlayerController::HitState);
 	AddState("dead", (statehandler)&TCompPlayerController::DeadState);
 
+	/* TODO: not for milestone1 */
+	//AddState("probe", (statehandler)&TCompPlayerController::ProbeState);
+	//AddState("push", (statehandler)&TCompPlayerController::PushState);
+
 	ChangeState("idle");
 }
 
-void TCompPlayerController::update(float dt) {
-  
-  //Guardo mi transform
-  TCompTransform *c_my_transform = get<TCompTransform>();
-
-  //----------------------------------------------
-  //Pongo a cero la velocidad actual
-  float amount_moved = speedFactor * dt;
-  float amount_rotated = rotationSpeed * dt;
-
-  // Current orientation
-  float current_yaw = 0.f;
-  float current_pitch = 0.f;
-  c_my_transform->getYawPitchRoll(&current_yaw, &current_pitch);
-
-  //Detecto el teclado
-  VEC3 local_speed = VEC3::Zero;
-  if (isPressed('W'))
-    local_speed.z += 1.f;
-  if (isPressed('S'))
-    local_speed.z -= 1.f;
-  if (isPressed('A'))
-    local_speed.x += 1.f;
-  if (isPressed('D'))
-    local_speed.x -= 1.f;
-  if (isPressed('Q'))
-    current_yaw += amount_rotated;
-  if (isPressed('E'))
-    current_yaw -= amount_rotated;
-
-  //const Input::TInterface_Keyboard& kb = CEngine::get().getInput().host(Input::PLAYER_1).keyboard();
-  const Input::TButton& bt = kb.key(VK_SPACE);
-  if (bt.getsPressed()) {
-    TEntityParseContext ctx;
-    if (parseScene("data/prefabs/bullet.prefab", ctx)) {
-      assert(!ctx.entities_loaded.empty());
-      // Send the entity who has generated the bullet
-      ctx.entities_loaded[0].sendMsg(TMsgAssignBulletOwner{ CHandle(this).getOwner() });
-    }
-  }
-
-  // Using TransformNormal because I just want to rotate
-  VEC3 world_speed = VEC3::TransformNormal(local_speed, c_my_transform->asMatrix());
-  // Guardo la y de la posicion y le sumo la nueva posicion a la x y a la z
-  VEC3 my_new_pos = c_my_transform->getPosition() + world_speed * amount_moved;
-  c_my_transform->setYawPitchRoll(current_yaw, current_pitch, 0.f);
-
-  //Actualizo la posicion del transform
-  c_my_transform->setPosition(my_new_pos);
-}
+//void TCompPlayerController::update(float dt) {
+//  
+//  //Guardo mi transform
+//  TCompTransform *c_my_transform = get<TCompTransform>();
+//
+//  //----------------------------------------------
+//  //Pongo a cero la velocidad actual
+//  float amount_moved = walkSpeedFactor * dt;
+//  float amount_rotated = rotationSpeed * dt;
+//
+//  // Current orientation
+//  float current_yaw = 0.f;
+//  float current_pitch = 0.f;
+//  c_my_transform->getYawPitchRoll(&current_yaw, &current_pitch);
+//
+//  //Detecto el teclado
+//  VEC3 local_speed = VEC3::Zero;
+//  if (isPressed('W'))
+//    local_speed.z += 1.f;
+//  if (isPressed('S'))
+//    local_speed.z -= 1.f;
+//  if (isPressed('A'))
+//    local_speed.x += 1.f;
+//  if (isPressed('D'))
+//    local_speed.x -= 1.f;
+//  if (isPressed('Q'))
+//    current_yaw += amount_rotated;
+//  if (isPressed('E'))
+//    current_yaw -= amount_rotated;
+//
+//  //const Input::TInterface_Keyboard& kb = CEngine::get().getInput().host(Input::PLAYER_1).keyboard();
+//  const Input::TButton& bt = kb.key(VK_SPACE);
+//  if (bt.getsPressed()) {
+//    TEntityParseContext ctx;
+//    if (parseScene("data/prefabs/bullet.prefab", ctx)) {
+//      assert(!ctx.entities_loaded.empty());
+//      // Send the entity who has generated the bullet
+//      ctx.entities_loaded[0].sendMsg(TMsgAssignBulletOwner{ CHandle(this).getOwner() });
+//    }
+//  }
+//
+//  // Using TransformNormal because I just want to rotate
+//  VEC3 world_speed = VEC3::TransformNormal(local_speed, c_my_transform->asMatrix());
+//  // Guardo la y de la posicion y le sumo la nueva posicion a la x y a la z
+//  VEC3 my_new_pos = c_my_transform->getPosition() + world_speed * amount_moved;
+//  c_my_transform->setYawPitchRoll(current_yaw, current_pitch, 0.f);
+//
+//  //Actualizo la posicion del transform
+//  c_my_transform->setPosition(my_new_pos);
+//}
 
 void TCompPlayerController::registerMsgs() {
 
@@ -97,7 +100,10 @@ void TCompPlayerController::registerMsgs() {
 
 
 void TCompPlayerController::IdleState(float dt){
-	if(kb.key(0x57).getsPressed()){
+	if (btShadowMerging.isPressed() && checkShadows()) {
+		ChangeState("smEnter");
+	}
+	else if (motionButtonsPressed()) {
 		ChangeState("motion");
 	}
 }
@@ -105,18 +111,16 @@ void TCompPlayerController::IdleState(float dt){
 
 
 void TCompPlayerController::MotionState(float dt){ 
-
-	if (!kb.key(0x57).isPressed()) {
-
+	if (!motionButtonsPressed()) {
 		ChangeState("idle");
 	}
 	else {
-		VEC3 local_speed = VEC3::Zero;
-		local_speed.z += 1.f;
+		
+		movePlayer(dt);
 
-		TCompTransform *c_my_transform = get<TCompTransform>();
-
-		c_my_transform->setPosition(c_my_transform->getPosition() + local_speed * dt);
+		if (btShadowMerging.isPressed()) {
+			ChangeState("smEnter");
+		}
 	}
 }
 
@@ -142,12 +146,16 @@ void TCompPlayerController::RemovingInhibitorState(float dt){
 
 
 void TCompPlayerController::ShadowMergingEnterState(float dt){
-
+	TCompRender* t = get<TCompRender>();
+	t->color = VEC4(0, 0, 0, 0);
+	ChangeState("smHor");
 }
 
 
 void TCompPlayerController::ShadowMergingHorizontalState(float dt){ 
-
+	if (!btShadowMerging.isPressed()) {
+		ChangeState("smExit");
+	}
 }
 
 
@@ -167,7 +175,9 @@ void TCompPlayerController::ShadowMergingLandingState(float dt){
 
 
 void TCompPlayerController::ShadowMergingExitState(float dt){
-
+	TCompRender* t = get<TCompRender>();
+	t->color = VEC4(1, 1, 1, 1);
+	ChangeState("idle");
 }
 
 
@@ -188,5 +198,38 @@ void TCompPlayerController::HitState(float dt){
 
 void TCompPlayerController::DeadState(float dt){
 
+}
+
+
+bool TCompPlayerController::motionButtonsPressed() {
+	return btUp.isPressed() || btDown.isPressed() || btLeft.isPressed() || btRight.isPressed();
+}
+
+bool TCompPlayerController::checkShadows() {
+
+	/* TODO */
+	return true;
+}
+
+void TCompPlayerController::movePlayer(float dt) {
+	//Guardo mi transform
+	TCompTransform *c_my_transform = get<TCompTransform>();
+
+	//----------------------------------------------
+	//Pongo a cero la velocidad actual
+	float amount_moved = walkSpeedFactor * dt;
+
+	//Detecto el teclado
+	VEC3 local_speed = VEC3::Zero;
+	if (btUp.isPressed())
+		local_speed.z += 1.f;
+	if (btDown.isPressed())
+		local_speed.z -= 1.f;
+	if (btLeft.isPressed())
+		local_speed.x += 1.f;
+	if (btRight.isPressed())
+		local_speed.x -= 1.f;
+
+	c_my_transform->setPosition(c_my_transform->getPosition() + local_speed * amount_moved);
 }
 
--- source/components/comp_player_controller.h
+++ source/components/comp_player_controller.h
@@ -8,23 +8,52 @@
 class TCompPlayerController : public IAIController {
 
 	/* Attributes */
-  float   speedFactor = 1.0f;
-  float   rotationSpeed = 1.0f;
-  VEC3    speed;
+
+	float stamina = 100.f;
+	float maxStamina = 100.f;
+	float minStamina = 0.f;
+	float dcrStaminaOnPlaceMultiplier = 0.5f;
+	float dcrStaminaGround = 0.02f;
+	float dcrStaminaWall = 0.08f;
+	float incrStamina = 0.5f;
+
+	float walkSpeedFactor = 1.0f;
+	float runSpeedFactor = 2.0f;
+	float walkSlowSpeedFactor = 0.5f;
+	float rotationSpeed = 1.0f;
+	VEC3  speed;
 
   /* Timers */
 
   /* Keys */
-  const Input::TInterface_Keyboard& kb = CEngine::get().getInput().host(Input::PLAYER_1).keyboard();
+  const Input::TButton& btUp = EngineInput["btUp"];
+  const Input::TButton& btDown = EngineInput["btDown"];
+  const Input::TButton& btRight = EngineInput["btRight"];
+  const Input::TButton& btLeft = EngineInput["btLeft"];
+  const Input::TButton& btShadowMerging = EngineInput["btShadowMerging"];
+  const Input::TButton& btAttack = EngineInput["btAttack"];
+  const Input::TButton& btRun = EngineInput["btRun"];
+  const Input::TButton& btSlow = EngineInput["btSlow"];
+  const Input::TButton& btAction = EngineInput["btAction"];
+  /* TODO: not for milestone 1 */
+  //const Input::TButton& btCrouch = EngineInput["w"];	
+  //const Input::TButton& btSecAction = EngineInput["w"];
+
+
+
 
   DECL_SIBLING_ACCESS();
 
   void onMsgDamage(const TMsgDamage& msg);
 
+  /* Private aux functions */
+  bool motionButtonsPressed();
+  void movePlayer(float);
+
 public:
   void debugInMenu();
   void load(const json& j, TEntityParseContext& ctx);
-  void update(float dt);
+  //void update(float dt);
   void Init();
   
   static void registerMsgs();
@@ -46,4 +75,6 @@ public:
   void LandingState(float);
   void HitState(float);
   void DeadState(float);
+
+  bool checkShadows();
 };
