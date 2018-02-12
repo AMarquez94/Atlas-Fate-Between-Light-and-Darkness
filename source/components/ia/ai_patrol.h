2bd3cfaacf6c13a91aba92c1a2a77705d8303407
Alejandro Márquez <alejandromarquez94@gmail.com>
2018-02-13 00:12:39 +0100

    WIP: Creating ai_patrol states

--- source/components/ia/ai_patrol.h
+++ source/components/ia/ai_patrol.h
@@ -8,26 +8,26 @@ class CAIPatrol : public IAIController
 {
 
 	/* Atributes */
-  std::vector<VEC3> _waypoints;
-  int currentWaypoint;
-  float speed;
-  float rotationSpeed;
-  float fov;
-  float distToIdleWar;
-  float distToBack;
-  float distToChase;
-  std::string entityToChase;
-  int life;
+  //std::vector<VEC3> _waypoints;
+  //int currentWaypoint;
+  //float speed;
+  //float rotationSpeed;
+  //float fov;
+  //float distToIdleWar;
+  //float distToBack;
+  //float distToChase;
+  //std::string entityToChase;
+  //int life;
 
   /* Timers */
-  float idleWarTimer = 0.f;
-  float idleWarTimerMax = 0.f;
-  float idleWarTimerBase;
-  int idleWarTimerExtra;
-  float currentOrbitRotation = 0.f;
-  float orbitRotationMax = 0.f;
-  float orbitRotationBase;
-  int orbitRotationExtra;
+  //float idleWarTimer = 0.f;
+  //float idleWarTimerMax = 0.f;
+  //float idleWarTimerBase;
+  //int idleWarTimerExtra;
+  //float currentOrbitRotation = 0.f;
+  //float orbitRotationMax = 0.f;
+  //float orbitRotationBase;
+  //int orbitRotationExtra;
 
   DECL_SIBLING_ACCESS();
 
@@ -37,13 +37,27 @@ public:
   void load(const json& j, TEntityParseContext& ctx);
   void debugInMenu();
 
-  void IdleState(float);
-  void SeekWptState(float);
-  void NextWptState(float);
-  void ClosestWptState(float);
-  void ChaseState(float);
-  void IdleWarState(float);
-  void ChooseOrbitSideState(float);
+  void IdleState(float);				//Does nothing
+  void SeekWptState(float);				//Moves to currentWpt
+  void WaitInWptState(float);			//Rotates to the desired position and wait for x time
+  void NextWptState(float);				//Calculates next currentWpt
+  void ClosestWptState(float);			//Calculates the closest waypoint
+  void SuspectState(float);				//The enemy thinks it may have seen the player. More or less time depending different factors
+  void ShootInhibitorState(float);		//The enemy shoots the inhibitor to the player
+  void ChaseState(float);				//Goes towards the player's position				
+  void AttackState(float);				//Attack the player
+  void IdleWarState(float);				//Waiting before attacks
+  void BeginAlertState(float);			//Turn on lintern. Enter in alert state (easy to see the player)
+  void GoToNoiseState(float);			//The enemy goes to the location where he heard something
+  void GoToPatrolState(float);			//The enemy goes to the location of a stunned enemy in order to repair it
+  void FixOtherPatrolState(float);		//The enemy fixes another enemy
+  void GoPlayerLastPosState(float);		//The enemy goes to the last position he has seen the player
+  void SeekPlayerState(float);			//The enemy keeps searching the player a bit more
+  void StunnedState(float);				//The enemy has been stunned by the player
+  void FixedState(float);				//The enemy has been fixed by another enemy
+  void ShadowMergedState(float);		//The enemy has been shadowmerged by the player
+  
+  void ChooseOrbitSideState(float);		
   void OrbitLeftState(float);
   void OrbitRightState(float);
   void BackState(float);
