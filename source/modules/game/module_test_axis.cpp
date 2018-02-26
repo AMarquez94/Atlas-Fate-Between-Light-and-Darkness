82fbbc987245d9a9b8c806688556e7ca039a5797
telojuriol <uribcn1994@gmail.com>
2018-02-26 17:21:17 +0100

    MERGE: Insert capsules.scene

--- source/modules/game/module_test_axis.cpp
+++ source/modules/game/module_test_axis.cpp
@@ -42,6 +42,10 @@ bool CModuleTestAxis::start()
 	  TEntityParseContext ctx;
 	  parseScene("data/scenes/lights.scene", ctx);
   }
+  {
+	  TEntityParseContext ctx;
+	  parseScene("data/scenes/capsules.scene", ctx);
+  }
  
   camera.lookAt(VEC3(12.0f, 8.0f, 8.0f), VEC3::Zero, VEC3::UnitY);
   camera.setPerspective(60.0f * 180.f / (float)M_PI, 0.1f, 1000.f);
