b3de2eaded3f0550d6bfa17db579c0e8844ad368
lord semi <josemi.masip@gmail.com>
2018-02-27 14:38:21 +0100

    FEATURE: Dynamic Capsules finally working ! Implemented dynamic creation at the start. Deleted deprecated component dynamic_cycle. Adjusted some values.

--- source/components/dynamic_objects/comp_dynamic_objects.cpp
+++ source/components/dynamic_objects/comp_dynamic_objects.cpp
@@ -27,32 +27,20 @@ void TCompDynamicObjects::debugInMenu() {
 	ImGui::DragFloat("Speed", &_speed, 0.1f, 0.f, 10.f);
 	ImGui::DragInt("Current Waypoint", &_currentWaypoint, 0, 0, 10);
 }
+void TCompDynamicObjects::StartCycle() {
+	if (!_start) {
+		TCompTransform *mypos = get<TCompTransform>();
+		/*float y, r, p;
+		mypos->getYawPitchRoll(&y, &p, &r);
+		float deltaYaw = mypos->getDeltaYawToAimTo(getWaypoint());
+		y = deltaYaw;
+		mypos->setYawPitchRoll(y, p, r);*/
+		mypos->setPosition(getWaypoint());
 
-
-void TCompDynamicObjects::load(const json& j, TEntityParseContext& ctx) {
-	Init();
-	//int a = j.count("waypoints");
-	assert(j.count("waypoints") >= 1);
-	auto& j_waypoints = j["waypoints"];
-	for (auto it = j_waypoints.begin(); it != j_waypoints.end(); ++it) {
-
-		VEC3 wpt;
-		assert(it.value().count("position") == 1);
-
-		wpt = loadVEC3(it.value()["position"]);
-		
-		addWaypoint(wpt);
+		_currentWaypoint++;
+		_start = true;
 	}
-	_speed = j.value("speed", 2.0f);
-	_rotationSpeed = deg2rad(j.value("rotationSpeed", 90));
-
-
 }
-
-void TCompDynamicObjects::Init() {
-	_currentWaypoint = 1;
-}
-
 void TCompDynamicObjects::rotateTowardsVec(VEC3 objective, float dt) {
 	/*TCompTransform *c_my_transform = get<TCompTransform>();	float y, r, p;
 	c_my_transform->getYawPitchRoll(&y, &p, &r);
@@ -79,35 +67,75 @@ void TCompDynamicObjects::rotateTowardsVec(VEC3 objective, float dt) {
 }
 
 
-void TCompDynamicObjects::StartCycle(){
-	if (!_start) {
-		TCompTransform *mypos = get<TCompTransform>();
-		/*float y, r, p;
-		mypos->getYawPitchRoll(&y, &p, &r);
-		float deltaYaw = mypos->getDeltaYawToAimTo(getWaypoint());
-		y = deltaYaw;
-		mypos->setYawPitchRoll(y, p, r);*/
-		mypos->setPosition(getWaypoint());
+void TCompDynamicObjects::load(const json& j, TEntityParseContext& ctx) {
+	//int a = j.count("waypoints");
+	assert(j.count("waypoints") >= 1);
+	auto& j_waypoints = j["waypoints"];
+	for (auto it = j_waypoints.begin(); it != j_waypoints.end(); ++it) {
+
+		VEC3 wpt;
+		assert(it.value().count("position") == 1);
+
+		wpt = loadVEC3(it.value()["position"]);
 		
-		_currentWaypoint++;
-		_start = true;
+		addWaypoint(wpt);
+	}
+	_speed = j.value("speed", 2.0f);
+	_rotationSpeed = deg2rad(j.value("rotationSpeed", 90));
+
+	assert(_waypoints.size() == 2);
+	_dir = _waypoints[1] - _waypoints[0];
+	_dir.Normalize();
+
+
+	Init();
+}
+
+void TCompDynamicObjects::Init() {
+	_currentWaypoint = 1;
+	for (auto it = 0; it < _waypoints.size() - 1; it++) {
+		_totalDistance += VEC3::Distance(_waypoints[it], _waypoints[it + 1]);
+	}
+	_numClones = _totalDistance / (_width + _offset);
+	_amountMoved = _width + _offset;
+	for (int it = 0; it < _numClones; it++) {
+		CalculatePositions(it);
+		TEntityParseContext ctx;
+		parseScene("data/prefabs/capsule.prefab", ctx);
+		CreateClone(ctx, it);
 	}
 }
-void TCompDynamicObjects::update(float dt) {
+
+void TCompDynamicObjects::CalculatePositions(int foo) {
+	_pos.push_back(_waypoints[0] + _dir * _amountMoved * foo);
+ }
+
+void TCompDynamicObjects::CreateClone(TEntityParseContext ctx, int it) {
+	CEntity* bar = ctx.entities_loaded[0];
+	CEntity* foo = bar;
+	_clones.push_back(foo);
+	TCompTransform *mypos = _clones[it]->get<TCompTransform>();
+
+
+	mypos->setPosition(_pos[it]);
+	/*if (it > 0){
+		dbg("The previous position for the clone number %d is %f %f %f \n", it-1, _pos[it-1].x, _pos[it-1].y, _pos[it-1].z);
+		dbg("The position for the clone number %d is %f %f %f \n", it, _pos[it].x, _pos[it].y, _pos[it].z);
+	}*/
+}
+
+void TCompDynamicObjects::Move(TCompTransform* mypos, float dt) {
 	//StartCycle();
 	if (!_start) {
-		TCompTransform *mypos = get<TCompTransform>();
 		if (_currentWaypoint != _waypoints.size()) {
 			//rotateTowardsVec(getWaypoint(), dt);
 			VEC3 vp = mypos->getPosition();
 			//Miramos si estamos lo suficientemente cerca para no pasarnos de largo
+			//Si es así
 			if (VEC3::Distance(getWaypoint(), vp) < _speed * dt) {
+				_currentWaypoint = 0;
 				mypos->setPosition(getWaypoint());
 				_currentWaypoint++;
-				if (_currentWaypoint == _waypoints.size()) {
-					_currentWaypoint = 0;
-					mypos->setPosition(getWaypoint());
-				}
 			}
 			else {
 				VEC3 dir = getWaypoint() - mypos->getPosition();
@@ -117,5 +145,15 @@ void TCompDynamicObjects::update(float dt) {
 			}
 		}
 	}
-	
+}
+
+void TCompDynamicObjects::update(float dt) {
+	TCompTransform* mypos = get<TCompTransform>();
+	Move(mypos, dt);
+	for (int it = 0; it < _clones.size(); it++) {
+		CHandle aux = _clones[it];
+		CEntity* aux2 = aux;
+		TCompTransform* mypos2 = aux2->get<TCompTransform>();
+		Move(mypos2, dt);
+	}	
 }
--- source/components/dynamic_objects/comp_dynamic_objects.h
+++ source/components/dynamic_objects/comp_dynamic_objects.h
@@ -9,19 +9,45 @@ class TCompDynamicObjects : public TCompBase {
 	
 	/* Atributes */
 	std::vector<VEC3> _waypoints;
+	//Current Waypoint
 	int _currentWaypoint;
+	//Speed
 	float _speed;
+	//Rotation Speed
 	float _rotationSpeed;
+	//Can we start?
 	bool _start = false;
+	//Total distance
+	float _totalDistance = 0.0f;
+	//Space inbetween objects
+	float _offset = 5.0f;
+	//Width of the capsules hardcoded TO-DO: Get this from the collider
+	float _width = 2.0f;
+	//Number of clones to be created
+	int _numClones;
+	//Amount of the instance moved
+	float _amountMoved = 0.0f;
+	//Direction vector
+	VEC3 _dir;
+	//Positions of each instance
+	std::vector <VEC3> _pos;
+	//Clones
+	std::vector <CEntity*> _clones;
 
 	void rotateTowardsVec(VEC3 objective, float dt);
 
 public:
 	void debugInMenu();
-	void load(const json& j, TEntityParseContext& ctx);
-	void Init();
+	//Retrieve waypoints, speed, rotation speed
+	void load(const json& j, TEntityParseContext& ctx); 
+	//Initialize information
+	void Init(); 
 	void update(float dt);
 	void StartCycle();
+	void CalculatePositions(int);
+	void CreateClone(TEntityParseContext, int);
+	void Move(TCompTransform*, float);
+
 
 	void addWaypoint(VEC3 wpt) { _waypoints.push_back(wpt); };
 	VEC3 getWaypoint() { return _waypoints[_currentWaypoint]; }
