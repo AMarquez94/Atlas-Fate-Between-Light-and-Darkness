9e5971b44169f91d570ce27c4eb93a77db8da762
telojuriol <uribcn1994@gmail.com>
2018-04-20 16:59:27 +0200

    WIP: upsi mabia oblidau jeje

--- source/components/player_controller/comp_player_animator.h
+++ source/components/player_controller/comp_player_animator.h
@@ -9,7 +9,7 @@ class TCompPlayerAnimator;
 class TCompPlayerAnimator : public TCompAnimator
 {
 public:
-	enum EAnimation { IDLE = 0, WALK, ATTACK, DEATH, CROUCH_IDLE, CROUCH_WALK, RUN , HIT_FRONT, HIT_BACK};
+	enum EAnimation { IDLE = 0, WALK, ATTACK, DEATH, DEAD, CROUCH_IDLE, CROUCH_WALK, RUN , HIT_FRONT, HIT_BACK, FALL, LAND_SOFT};
 	struct TMsgExecuteAnimation {
 
 		TCompPlayerAnimator::EAnimation animation;
