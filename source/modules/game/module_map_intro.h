c09d9dbcf5e479ad8d00e8ab30bd324ad5dbc1ad
albertoamo <alberto93.dev@gmail.com>
2018-05-15 18:23:39 +0200

    FEATURE: Adding changes so that the scene is properly loaded.

--- source/modules/game/module_map_intro.h
+++ source/modules/game/module_map_intro.h
@@ -6,9 +6,9 @@ class CModuleMapIntro : public IModule
 {
 
 public:
-	CModuleMapIntro(const std::string& aname) : IModule(aname) { }
-  bool start() override;
-  bool stop() override;
-  void update(float delta) override;
-  void render() override;
+    CModuleMapIntro(const std::string& aname) : IModule(aname) { }
+    bool start() override;
+    bool stop() override;
+    void update(float delta) override;
+    void render() override;
 };
