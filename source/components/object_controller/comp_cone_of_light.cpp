a3a2b1dece8c98d9bd0eb5bb3615e4eb40e65264
Alejandro Márquez <alejandromarquez94@gmail.com>
2018-08-01 20:48:47 +0200

    FIX: Patrol lantern to illuminate player when it has to.

--- source/components/object_controller/comp_cone_of_light.cpp
+++ source/components/object_controller/comp_cone_of_light.cpp
@@ -16,8 +16,8 @@ void TCompConeOfLightController::debugInMenu() {
 }
 
 void TCompConeOfLightController::load(const json& j, TEntityParseContext& ctx) {
-    hor_fov = deg2rad(j.value("fov", 70.f));
-    ver_fov = deg2rad(j.value("fov", 45.f));
+    hor_fov = deg2rad(j.value("hor_fov", 70.f));
+    ver_fov = deg2rad(j.value("ver_fov", 70.f));
     dist = j.value("dist", 10.f);
     h_player = getEntityByName(j.value("target", "The Player"));
     turnedOn = j.value("turnedOn", false);
@@ -42,9 +42,14 @@ void TCompConeOfLightController::update(float dt) {
         TCompTransform* ppos = player->get<TCompTransform>();
         TCompTransform* mypos = get<TCompTransform>();
         bool inDist = VEC3::Distance(mypos->getPosition(), ppos->getPosition()) < dist;
-        if (VEC3::Distance(mypos->getPosition(), ppos->getPosition()) < dist
-            && mypos->isInFov(ppos->getPosition(), hor_fov, ver_fov)) {
-            if (!isPlayerHiddenFromLight()) {
+        //dbg("======================================\n");
+        //dbg("Is in dist %s\n", inDist ? "True" : "False");
+        bool inFov = mypos->isInFov(ppos->getPosition(), hor_fov, ver_fov);
+        //dbg("Is in fov %s\n", inFov ? "True" : "False");
+        if (inDist && inFov) {
+            bool isHidden = isPlayerHiddenFromLight();
+            //dbg("Is hidden %s\n", isHidden ? "True" : "False");
+            if (!isHidden) {
                 isPlayerIlluminatedNow = true;
                 if (!playerIlluminated) {
                     playerIlluminated = true;
