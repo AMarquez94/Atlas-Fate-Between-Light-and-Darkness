05fa45ef7e13b717efea29c83d1861a452d2f2c9
telojuriol <uribcn1994@gmail.com>
2018-08-06 11:39:44 +0200

    FEATURE: Fixing errors

--- source/components/object_controller/comp_laser.cpp
+++ source/components/object_controller/comp_laser.cpp
@@ -15,12 +15,11 @@ void TCompLaser::load(const json& j, TEntityParseContext& ctx) {
 void TCompLaser::update(float dt) {
 	CEntity* e = CHandle(this).getOwner();
 	TCompTransform *mypos = e->get<TCompTransform>();
-	dbg("%f\n",mypos->getUp().y);
 	physx::PxRaycastHit hit;
 	VEC3 origin = mypos->getPosition();
 	VEC3 dir = mypos->getFront();
 	if (EnginePhysics.Raycast(origin, dir, 40.0f, hit, physx::PxQueryFlag::eSTATIC)) {
-		//mypos->setScale( VEC3( 1, 1, (float)hit.distance ) );
+		mypos->setScale( VEC3( 1, 1, (float)hit.distance ) );
 	}
 		
 }
