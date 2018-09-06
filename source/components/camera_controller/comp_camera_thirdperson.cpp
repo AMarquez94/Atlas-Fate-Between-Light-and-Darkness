af1b3f2bc4ae3c295d926a08e68b133c21b8b945
albertoamo <alberto93.dev@gmail.com>
2018-09-06 21:04:18 +0200

    FEATURE: Adding the cameras to the prefab.

--- source/components/camera_controller/comp_camera_thirdperson.cpp
+++ source/components/camera_controller/comp_camera_thirdperson.cpp
@@ -24,15 +24,6 @@ void TCompCameraThirdPerson::load(const json& j, TEntityParseContext& ctx)
     _clipping_offset = loadVEC3(j["offset"]);
     _clamp_angle = VEC2(deg2rad(_clamp_angle.x), deg2rad(_clamp_angle.y));
 
-    // Load the target and set his axis as our axis.
-    _h_target = ctx.findEntityByName(_target_name);
-    TCompTransform* target_transform = ((CEntity*)_h_target)->get<TCompTransform>();
-
-    float yaw, pitch, roll;
-    target_transform->getYawPitchRoll(&yaw, &pitch, &roll);
-    _current_euler = VEC2(yaw, pitch);
-    _original_euler = _current_euler;
-
     active = false;
 }
 
@@ -44,6 +35,8 @@ void TCompCameraThirdPerson::registerMsgs()
     DECL_MSG(TCompCameraThirdPerson, TMsgSetCameraActive, onMsgCameraSetActive);
     DECL_MSG(TCompCameraThirdPerson, TMsgScenePaused, onPause);
     DECL_MSG(TCompCameraThirdPerson, TMsgCameraReset, onMsgCameraReset);
+    DECL_MSG(TCompCameraThirdPerson, TMsgEntityCreated, onMsgCameraCreated);
+    DECL_MSG(TCompCameraThirdPerson, TMsgEntitiesGroupCreated, onMsgCameraGroupCreated);
 }
 
 void TCompCameraThirdPerson::onMsgCameraActive(const TMsgCameraActivated & msg)
@@ -61,6 +54,30 @@ void TCompCameraThirdPerson::onMsgCameraActive(const TMsgCameraActivated & msg)
             v_tp_cameras[i].sendMsg(msg);
         }
     }
+}
+
+void TCompCameraThirdPerson::onMsgCameraGroupCreated(const TMsgEntitiesGroupCreated & msg) {
+
+    // Load the target and set his axis as our axis.
+    _h_target = getEntityByName(_target_name);
+    TCompTransform* target_transform = ((CEntity*)_h_target)->get<TCompTransform>();
+
+    float yaw, pitch, roll;
+    target_transform->getYawPitchRoll(&yaw, &pitch, &roll);
+    _current_euler = VEC2(yaw, pitch);
+    _original_euler = _current_euler;
+}
+
+void TCompCameraThirdPerson::onMsgCameraCreated(const TMsgEntityCreated & msg) {
+
+    // Load the target and set his axis as our axis.
+    _h_target = getEntityByName(_target_name);
+    TCompTransform* target_transform = ((CEntity*)_h_target)->get<TCompTransform>();
+
+    float yaw, pitch, roll;
+    target_transform->getYawPitchRoll(&yaw, &pitch, &roll);
+    _current_euler = VEC2(yaw, pitch);
+    _original_euler = _current_euler;
 }
 
 void TCompCameraThirdPerson::onMsgCameraFullActive(const TMsgCameraFullyActivated & msg)
--- source/components/camera_controller/comp_camera_thirdperson.h
+++ source/components/camera_controller/comp_camera_thirdperson.h
@@ -7,39 +7,41 @@
 class TCompCameraThirdPerson : public CCamera, public TCompBase
 {
 private:
-	CHandle     _h_target;
-	std::string _target_name;
+    CHandle     _h_target;
+    std::string _target_name;
 
-	float _speed;
-	VEC2 _clamp_angle;
-	VEC2 _current_euler;
-  VEC2 _original_euler;
-	VEC3 _clipping_offset;
+    float _speed;
+    VEC2 _clamp_angle;
+    VEC2 _current_euler;
+    VEC2 _original_euler;
+    VEC3 _clipping_offset;
 
-	bool active;
+    bool active;
 
-	const Input::TButton& btHorizontal = EngineInput["MouseX"];
-	const Input::TButton& btVertical = EngineInput["MouseY"];
-	const Input::TButton& btDebugPause = EngineInput["btDebugPause"];
-	void onPause(const TMsgScenePaused& msg);
+    const Input::TButton& btHorizontal = EngineInput["MouseX"];
+    const Input::TButton& btVertical = EngineInput["MouseY"];
+    const Input::TButton& btDebugPause = EngineInput["btDebugPause"];
+    void onPause(const TMsgScenePaused& msg);
 
-	DECL_SIBLING_ACCESS();
+    DECL_SIBLING_ACCESS();
 
-	void onMsgCameraActive(const TMsgCameraActivated &msg);
-	void onMsgCameraFullActive(const TMsgCameraFullyActivated &msg);
-	void onMsgCameraDeprecated(const TMsgCameraDeprecated &msg);
-	void onMsgCameraSetActive(const TMsgSetCameraActive &msg);
-  void onMsgCameraReset(const TMsgCameraReset &msg);
+    void onMsgCameraActive(const TMsgCameraActivated &msg);
+    void onMsgCameraCreated(const TMsgEntityCreated &msg);
+    void onMsgCameraGroupCreated(const TMsgEntitiesGroupCreated &msg);
+    void onMsgCameraFullActive(const TMsgCameraFullyActivated &msg);
+    void onMsgCameraDeprecated(const TMsgCameraDeprecated &msg);
+    void onMsgCameraSetActive(const TMsgSetCameraActive &msg);
+    void onMsgCameraReset(const TMsgCameraReset &msg);
 
 
 public:
-  void debugInMenu();
-  void load(const json& j, TEntityParseContext& ctx);
-  void update(float dt);
+    void debugInMenu();
+    void load(const json& j, TEntityParseContext& ctx);
+    void update(float dt);
 
-  const VEC2 getCurrentEuler() { return _current_euler; }
-  float CameraClipping(const VEC3 & origin, const VEC3 & dir);
-  void setCurrentEuler(float euler_x = INFINITY, float euler_y = INFINITY);
+    const VEC2 getCurrentEuler() { return _current_euler; }
+    float CameraClipping(const VEC3 & origin, const VEC3 & dir);
+    void setCurrentEuler(float euler_x = INFINITY, float euler_y = INFINITY);
 
-  static void registerMsgs();
+    static void registerMsgs();
 };
