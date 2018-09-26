3d3291058269fe4959ba9dbbabd87999a9d51aa8
Alejandro Márquez <alejandromarquez94@gmail.com>
2018-09-26 13:52:38 +0200

    FIX: Buttons changed.

--- source/components/player_controller/comp_player_input.cpp
+++ source/components/player_controller/comp_player_input.cpp
@@ -91,15 +91,6 @@ void TCompPlayerInput::update(float dt)
                     e->sendMsg(attack);
                     attackButtonJustPressed = true;
                 }
-                else if (c_my_player->canSonarPunch()) {
-
-                    TMsgSetFSMVariable sonar;
-                    sonar.variant.setName("sonar");
-                    sonar.variant.setBool(true);
-                    e->sendMsg(sonar);
-                    attackButtonJustPressed = true;
-                }
-
             }
             else if (attackButtonJustPressed) {
                 TMsgSetFSMVariable attack;
@@ -107,12 +98,26 @@ void TCompPlayerInput::update(float dt)
                 attack.variant.setBool(false);
                 e->sendMsg(attack);
 
+                attackButtonJustPressed = false;
+            }
+
+            if (EngineInput["btSonar"].getsPressed()) {
+                TCompTempPlayerController* c_my_player = get<TCompTempPlayerController>();
+                if (c_my_player->canSonarPunch()) {
+                    TMsgSetFSMVariable sonar;
+                    sonar.variant.setName("sonar");
+                    sonar.variant.setBool(true);
+                    e->sendMsg(sonar);
+                    sonarButtonJustPressed = true;
+                }
+            }
+            else if (sonarButtonJustPressed) {
                 TMsgSetFSMVariable sonar;
                 sonar.variant.setName("sonar");
                 sonar.variant.setBool(false);
                 e->sendMsg(sonar);
 
-                attackButtonJustPressed = false;
+                sonarButtonJustPressed = false;
             }
 
             if (EngineInput["btCrouch"].getsPressed() && !EngineInput["btRun"].isPressed())
