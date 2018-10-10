47039acea0bcd4b5524a55a76076453e71a9cd38
Alejandro Márquez <alejandromarquez94@gmail.com>
2018-10-10 21:02:22 +0200

    FEATURE: Intro corridor to coliseo working.

--- Bin/data/scripts/Scenes/coliseo.lua
+++ Bin/data/scripts/Scenes/coliseo.lua
@@ -6,6 +6,8 @@ end
 function onScenePartialStart_scene_coliseo()
 	onSceneStart_scene_coliseo();
 	movePlayerToRefPos("col_intro_suelo001", i_ref_pos);
+	tdoor_intro_corridor = toDoor(toEntity(getEntityByName("col_intro_framedoor")):getCompByName("door"));
+	execScriptDelayed("tdoor_intro_corridor:open()", 0.5);
 	--tdoor_intro_corridor = toDoor(toEntity(getEntityByName("tech_door_intro")):getCompByName("door"));
 	--tdoor_intro_coliseo = toDoor(toEntity(getEntityByName("tech_door_intro")):getCompByName("door"));
 end
@@ -18,6 +20,25 @@ end
 function onScenePartialStart_scene_coliseo_2()
 	onSceneStart_scene_coliseo_2();
 	movePlayerToRefPos("col_intro_suelo001", i_ref_pos);
+end
+
+function onTriggerEnter_COL_trigger_corridor_intro_player()
+	closeIntroDoor();
+end
+
+function onTriggerEnter_COL_trigger_corridor_intro01_player()
+	closeIntroDoor();
+end
+
+function onTriggerEnter_COL_trigger_corridor_intro02_player()
+	closeIntroDoor();
+end
+
+function closeIntroDoor()
+	tdoor_intro_corridor:close();
+	getEntityByName("COL_trigger_corridor_intro"):destroy();
+	getEntityByName("COL_trigger_corridor_intro01"):destroy();
+	getEntityByName("COL_trigger_corridor_intro02"):destroy();
 end
 
 function transition_coliseum_to_zone_a()
