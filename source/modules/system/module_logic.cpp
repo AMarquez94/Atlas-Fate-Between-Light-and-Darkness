9ef43c754a0804dd9e45cee877a289ea893632aa
telojuriol <uribcn1994@gmail.com>
2018-10-19 19:25:32 +0200

    WIP: Closing lights on last scene

--- source/modules/system/module_logic.cpp
+++ source/modules/system/module_logic.cpp
@@ -1263,31 +1263,8 @@ void setOutBlackScreen(float time_to_lerp) {
 
 void lightUpForFinalScene(float time) {
 
-	/*EngineLerp.lerpElement(&cb_globals.global_exposure_adjustment,1.02, time,0);
-	EngineLerp.lerpElement(&cb_globals.global_fog_density, 0.185, time, 0);
-	CHandle h_cam = EngineCameras.getCurrentCamera();
-	CEntity *e_cam = h_cam;
-	TCompRenderBloom* comp_bloom = e_cam->get<TCompRenderBloom>();
-	EngineLerp.lerpElement(&comp_bloom->threshold_min, 0.170, time, 0);
-	EngineLerp.lerpElement(&comp_bloom->threshold_max, 0.490, time, 0);
-	EngineLerp.lerpElement(&comp_bloom->multiplier, 1.780, time, 0);
-	EngineLerp.lerpElement(&comp_bloom->add_weights.x, 1.480, time, 0);
-	EngineLerp.lerpElement(&comp_bloom->add_weights.y, 1.7, time, 0);
-	EngineLerp.lerpElement(&comp_bloom->add_weights.z, 0.360, time, 0);
-	EngineLerp.lerpElement(&comp_bloom->add_weights.w, 1.2, time, 0);*/
 	EngineEntities.broadcastMsg(TMsgEmisiveCapsuleState{false});
-	/*ImGui::DragFloat("Exposure Adjustment", &cb_globals.global_exposure_adjustment, 0.01f, 0.1f, 32.f);
-	ImGui::DragFloat("Ambient Adjustment", &cb_globals.global_ambient_adjustment, 0.01f, 0.0f, 1.f);
-	//ImGui::DragFloat("HDR", &cb_globals.global_hdr_enabled, 0.01f, 0.0f, 1.f);
-	ImGui::DragFloat("Gamma Correction", &cb_globals.global_gamma_correction_enabled, 0.01f, 0.0f, 1.f);
-	ImGui::DragFloat("Reinhard vs Uncharted2", &cb_globals.global_tone_mapping_mode, 0.01f, 0.0f, 1.f);
-	ImGui::DragFloat("Global shadow intensity", &cb_globals.global_shadow_intensity, 0.001f, 0.0f, 1.f);
-	// Fog settings edition
-	ImGui::DragFloat("Fog Ground density", &cb_globals.global_fog_ground_density, 0.001f, 0.0f, 1.f);
-	ImGui::DragFloat("Fog Environment density", &cb_globals.global_fog_density, 0.001f, 0.0f, 1.f);
-	ImGui::ColorEdit4("Shadow Color", &cb_globals.global_shadow_color.x, 0.0001f);
-	ImGui::ColorEdit4("Fog Ground Color", &cb_globals.global_fog_color.x, 0.0001f);
-	ImGui::ColorEdit4("Fog Environment Color", &cb_globals.global_fog_env_color.x, 0.0001f);*/
+	
 }
 
 void lightDownForFinalScene() {
