6dbb832cbb5f7246a61a73dd9ed0e907343fada6
telojuriol <uribcn1994@gmail.com>
2018-10-20 04:10:42 +0200

    WIP: particles fade out on final scene

---
+++ source/components/object_controller/particles_final_scene_controller.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "components/comp_base.h"
+#include "components/comp_particles.h"
+class TCompParticlesFinalSceneController : public TCompBase
+{
+
+	DECL_SIBLING_ACCESS();
+
+	bool active = false;
+	float time_to_destroy = 0.0f;
+	float current_time = 0.0f;
+
+public:
+	void debugInMenu();
+	void load(const json& j, TEntityParseContext& ctx);
+	void update(float dt);
+
+	static void registerMsgs();
+	void onMsgEntityCreated(const TMsgEntityCreated& msg);
+	void onMsgWheelRotating(const TMsgRotatorAccelerate& msg);
+};
